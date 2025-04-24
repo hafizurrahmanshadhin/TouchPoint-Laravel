804f6b37c5c130901deb97ce0608c93414fe2267
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-04-24 12:46:50 +0600

    proce number formate update

--- app/Http/Resources/Api/Subscription/SubscriptionPlanResource.php
+++ app/Http/Resources/Api/Subscription/SubscriptionPlanResource.php
@@ -15,7 +15,7 @@ class SubscriptionPlanResource extends JsonResource {
         return [
             'id'                => $this->id,
             'subscription_plan' => $this->subscription_plan,
-            'price'             => (float) number_format((float) $this->price, 2, '.', ''),
+            'price'             => (float) number_format($this->price, 2, '.', ''),
             'billing_cycle'     => $this->billing_cycle,
             'touch_points'      => $this->touch_points === null ? 'Unlimited Touchpoints' : $this->touch_points,
             'has_ads'           => (bool) $this->has_ads,
